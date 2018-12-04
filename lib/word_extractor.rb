13570e2251fac9c6dd0c861894318a5496f3239a
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-04 17:36:03 +0000

    #extract_from_file adds words to the word bank

--- lib/word_extractor.rb
+++ lib/word_extractor.rb
@@ -1,5 +1,13 @@
 class WordExtractor
+  def initialize(word_bank: WordBank.new)
+    @word_bank = word_bank
+  end
+
   def extract_from_file(file_name)
-    File.open("./#{file_name}", 'r')
+    File.open("./#{file_name}", 'r') do |file|
+      file.each_line do |word|
+        @word_bank.add(word)
+      end
+    end
   end
 end
--- spec/units/word_extractor_spec.rb
+++ spec/units/word_extractor_spec.rb
@@ -1,9 +1,17 @@
 require 'word_extractor'
 
 describe WordExtractor do
+  let(:word_bank) { double :word_bank}
+  let(:subject) { described_class.new(word_bank: word_bank) }
+
   describe '#extract_from_file' do
     it 'opens a file' do
       expect(File).to receive(:open)
+      subject.extract_from_file('word_list.txt')
+    end
+
+    it 'adds words to the word bank' do
+      expect(word_bank).to receive(:add).exactly(7).times
       subject.extract_from_file('word_list.txt')
     end
   end
