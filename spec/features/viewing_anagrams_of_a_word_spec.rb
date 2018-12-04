711e04145daedcc4f493d65b30e09d84fb57d43d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-04 19:02:57 +0000

    displays no words if anagrams are not found

--- spec/features/viewing_anagrams_of_a_word_spec.rb
+++ spec/features/viewing_anagrams_of_a_word_spec.rb
@@ -1,9 +1,17 @@
 describe 'Viewing anagrams of a word' do
-  it 'it displays all possible words when a word is entered' do
+  it 'displays all possible words when a word is entered' do
     allow_any_instance_of(Object).to receive(:gets).and_return("stop word_list.txt\n")
 
     expect { load 'main.rb' }.to output(
       "Enter a word\nThe anagrams are:\npost\npots\ntops\nopts\nspot\n"
+    ).to_stdout
+  end
+
+  it 'displays no words if anagrams are not found' do
+    allow_any_instance_of(Object).to receive(:gets).and_return("stop listen_list.txt\n")
+
+    expect { load 'main.rb' }.to output(
+      "Enter a word\nThe anagrams are:\n\n"
     ).to_stdout
   end
 end
