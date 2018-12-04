7b85632f891f4089a8ac0d1ee497f12481231c20
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-04 18:23:36 +0000

    updated to include word and filename in one input

--- spec/features/viewing_anagrams_of_a_word_spec.rb
+++ spec/features/viewing_anagrams_of_a_word_spec.rb
@@ -1,6 +1,7 @@
 describe 'Viewing anagrams of a word' do
   it 'it displays all possible words when a word is entered' do
-    allow_any_instance_of(Object).to receive(:gets).and_return("stop\n")
+    allow_any_instance_of(Object).to receive(:gets).and_return("stop word_list.txt\n")
+
     expect { load 'main.rb' }.to output(
       "Enter a word\nThe anagrams are:\npost\npots\ntops\nopts\nspot\n"
     ).to_stdout
