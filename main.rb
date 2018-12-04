c757e2640d5ea03367db0dd7e96c06e08ac7dcd2
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-04 15:54:28 +0000

    added prompts to UI

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
 require_relative 'lib/anagram_checker'
 
+puts 'Enter a word'
 input = gets.chomp
+
+puts "The anagrams are:"
 puts AnagramChecker.solve(input)
--- spec/features/viewing_anagrams_of_a_word_spec.rb
+++ spec/features/viewing_anagrams_of_a_word_spec.rb
@@ -2,7 +2,7 @@ describe 'Viewing anagrams of a word' do
   it 'it displays all possible words when a word is entered' do
     allow_any_instance_of(Object).to receive(:gets).and_return("stop\n")
     expect { load 'main.rb' }.to output(
-      "post\npots\ntops\nopts\nspot\n"
+      "Enter a word\nThe anagrams are:\npost\npots\ntops\nopts\nspot\n"
     ).to_stdout
   end
 end
