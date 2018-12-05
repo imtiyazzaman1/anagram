b5c9239232174837d7f69b7af122714662bf9834
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-05 11:29:47 +0000

    changed to use %W

--- spec/features/entering_invalid_file_name_spec.rb
+++ spec/features/entering_invalid_file_name_spec.rb
@@ -1,6 +1,6 @@
 describe 'Invalid file name' do
   context 'incorrect file extension' do
-    let(:user_input) { ["stop\n", "word_list\n"] }
+    let(:user_input) { %W[stop\n word_list\n] }
     it 'displays an error' do
       allow_any_instance_of(Object).to receive(:gets).and_return(*user_input)
       expect { load 'main.rb' }.to raise_error('Error! File not found')
