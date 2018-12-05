b25c910a587d13d1ca9736e00b20ec998c6d1eb2
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-05 11:07:56 +0000

    removed check for invalid characters

--- main.rb
+++ main.rb
@@ -2,15 +2,10 @@ require_relative 'lib/anagram_checker'
 
 def main
   checker = AnagramChecker.new
-
+  
   loop do
     puts 'Enter a word'
     word_input = gets.chomp
-
-    if contains_invalid_character?(word_input)
-      puts 'Error! Invalid character found'
-      break
-    end
 
     puts 'Enter the name of the file contain a list of words'
     file_input = gets.chomp
@@ -21,12 +16,6 @@ def main
     puts checker.solve(word_input)
     break
   end
-end
-
-private
-
-def contains_invalid_character?(word)
-  word[/[a-zA-Z]+/] != word
 end
 
 main
