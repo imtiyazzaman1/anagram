530d609054daf9d126c127e0af53b69eef68b99b
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-05 09:47:21 +0000

    assigned return value of gets to variable

--- spec/features/viewing_anagrams_of_a_word_spec.rb
+++ spec/features/viewing_anagrams_of_a_word_spec.rb
@@ -3,7 +3,7 @@ describe 'Viewing anagrams of a word' do
     let(:user_input) { ["stop\n", "word_list.txt\n"] }
 
     it 'displays all possible words' do
-      allow_any_instance_of(Object).to receive(:gets).and_return(*["stop\n", "word_list.txt\n"])
+      allow_any_instance_of(Object).to receive(:gets).and_return(*user_input)
       expect { load 'main.rb' }.to output(
         "Enter a word\nEnter the name of the file contain a list of words
 The anagrams are:\npost\npots\ntops\nopts\nspot\n"
@@ -12,10 +12,10 @@ The anagrams are:\npost\npots\ntops\nopts\nspot\n"
   end
 
   context 'when the file does not contain anagrams' do
+    let(:user_input) { ["stop\n", "listen_list.txt\n"] }
+
     it 'displays no words' do
-      allow_any_instance_of(Object).to receive(:gets).and_return(
-        *["stop\n", "listen_list.txt\n"]
-      )
+      allow_any_instance_of(Object).to receive(:gets).and_return(*user_input)
 
       expect { load 'main.rb' }.to output(
         "Enter a word\nEnter the name of the file contain a list of words
