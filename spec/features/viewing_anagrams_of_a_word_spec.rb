bfe1b47daf7becd031460ee4a396139039ebdb62
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-05 09:44:39 +0000

    added context block to make test more readable

--- spec/features/viewing_anagrams_of_a_word_spec.rb
+++ spec/features/viewing_anagrams_of_a_word_spec.rb
@@ -1,21 +1,26 @@
 describe 'Viewing anagrams of a word' do
-  let(:user_input) { ["stop\n", "word_list.txt\n"] }
-  it 'displays all possible words when a word is entered' do
-    allow_any_instance_of(Object).to receive(:gets).and_return(*["stop\n", "word_list.txt\n"])
-    expect { load 'main.rb' }.to output(
-      "Enter a word\nEnter the name of the file contain a list of words
+  context 'when the files contains anagrams' do
+    let(:user_input) { ["stop\n", "word_list.txt\n"] }
+
+    it 'displays all possible words' do
+      allow_any_instance_of(Object).to receive(:gets).and_return(*["stop\n", "word_list.txt\n"])
+      expect { load 'main.rb' }.to output(
+        "Enter a word\nEnter the name of the file contain a list of words
 The anagrams are:\npost\npots\ntops\nopts\nspot\n"
-    ).to_stdout
+      ).to_stdout
+    end
   end
 
-  it 'displays no words if anagrams are not found' do
-    allow_any_instance_of(Object).to receive(:gets).and_return(
-      *["stop\n", "listen_list.txt\n"]
-    )
+  context 'when the file does not contain anagrams' do
+    it 'displays no words' do
+      allow_any_instance_of(Object).to receive(:gets).and_return(
+        *["stop\n", "listen_list.txt\n"]
+      )
 
-    expect { load 'main.rb' }.to output(
-      "Enter a word\nEnter the name of the file contain a list of words
+      expect { load 'main.rb' }.to output(
+        "Enter a word\nEnter the name of the file contain a list of words
 The anagrams are:\n\n"
-    ).to_stdout
+      ).to_stdout
+    end
   end
 end
